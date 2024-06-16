093b0c3fe3d4de5dad99cee618219ce2fc0bc6a9
HarisRaa <naufal.122140040@student.itera.ac.id>
2024-06-16 09:50:17 +0700

    progress

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
-let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll('section')
+let navlinks = document.querySelectorAll('header nav a')
 
 window.onscroll = () => {
   sections.forEach(sec => {
@@ -11,8 +11,10 @@ window.onscroll = () => {
     if(top >= offset && top < offset + height) {
       navlinks.forEach(links => {
         links.classList.remove('active');
-        document.querySelector('header nav a [href*=' + id +']').classList.add('active');
       });
+      document.querySelector(`header nav a[href='#${id}']`).classList.add('active');
     };
   });
 };
+
+let haloUser = document.getElementById('Username');
